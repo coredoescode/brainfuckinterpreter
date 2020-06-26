b751eefd49b433a2ee1723dd40c236e9de51872d
GhostlyCoding <pokeball.181818@gmail.com>
2020-06-25 19:24:15 -0500

    Changed main.cpp and created interpreter.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include "cxxopts.hpp"
+#include "interpreter.cpp"
 
 int main(int argc, char **argv) {
     cxxopts::Options options("BFInterpreter", "A random bf interpreter made by three nerds in discord.");
@@ -12,8 +14,11 @@ int main(int argc, char **argv) {
     file_path = opts["file"].as<string>();
 
     ifstream file (file_path);
+    string fileText
     if (file.is_open()) {
-
+	ostringstream ss;
+	ss << file.rdbuf();
+	fileText = ss.str();
     } else {
 	    cout << "Unable to open file " << file_path << endl;
 	    return -1;
