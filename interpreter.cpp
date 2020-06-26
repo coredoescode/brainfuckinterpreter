312588886e5920e432a46469eb5922d5b1f47cc2
GhostlyCoding <pokeball.181818@gmail.com>
2020-06-25 19:53:45 -0500

    Changed interpreter.cpp

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+#include "transpiler.cpp"
 void interpret(std::string text){
-	
+	for(char letter : text){
+		switch(letter)
+		case '>':
+			increasePointerLocation();
+			break;
+		case '<':
+			decreasePointerLocation();
+			break;
+		case '+':
+			inceaseValueAtPointer();
+			break;
+		case '-':
+			decreaseValueAtPointer();
+			break;
+		case '[':
+			ifZeroJumpToClosingBracket();
+			break;
+		case ']':
+			ifNotZeroJumpToClosingBracket();
+			break;
+		case '.':
+			printPointerToOutput();
+			break;
+		case ',':
+			inputCharacterToPointer();
+			break;
+		
+	}
 }
